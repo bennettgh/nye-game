1b6090edb71d11f6586dfa17f47ccfb16f5ff4ed
Lucas Watts <lucaswatts41@gmail.com>
2024-12-20 00:05:38 -0500

    extra prompts

--- game-server/config/prompt.ts
+++ game-server/config/prompt.ts
@@ -4,6 +4,10 @@ export const prompts: string[] = [
   "The person closest to the TV got you a gift - it's a _______",
   "Knock knock knock, ______ is at the door",
   "Frog had a rough day at Sobotec, and ordered _______ on UberEats",
-  "Clap clap clap on a poodles fat ass _____",
+  "Clap clap clap on a poodles fat _____",
   "You ordered a burger and accidentally got this as a condiment",
+  "You're spending a night at the Millards, but the beds are full. You just found a place to sleep. Where is it?",
+  "Zoonie calls you to tell you about his day. What happened?",
+  "Fil watched a new documentary. What lifestyle changes is he making?",
+  "Doni got a new dog. What's does Fil call it?",
 ];
