9359c0ff3a64089bacfd4608283c74065ab6dab4
sisiandrew <andrew@sisiagency.com>
2024-12-20 14:27:09 -0500

    first round special

--- game-app/src/renderer/src/pages/Game/Intro.tsx
+++ game-app/src/renderer/src/pages/Game/Intro.tsx
@@ -1,8 +1,40 @@
 import { DevButton } from '@renderer/components/DevButton'
 import { GradientBackground } from '@renderer/components/GradientBackground'
+import { Title } from '@renderer/components/Title'
 import { Game } from '@renderer/context/types'
 
 export const Intro = ({
+  gameState,
+  handleEndPhase
+}: {
+  gameState: Game
+  handleEndPhase: () => void
+}) => {
+  const isFirstRound = gameState.rounds.length === 1
+
+  if (isFirstRound) {
+    return <FirstRoundIntro gameState={gameState} handleEndPhase={handleEndPhase} />
+  }
+
+  return <NormalRoundIntro gameState={gameState} handleEndPhase={handleEndPhase} />
+}
+
+const FirstRoundIntro = ({
+  gameState,
+  handleEndPhase
+}: {
+  gameState: Game
+  handleEndPhase: () => void
+}) => {
+  return (
+    <GradientBackground>
+      <Title>Welcome to BEANS EXTREMES</Title>
+      <DevButton onClick={handleEndPhase}>End phase</DevButton>
+    </GradientBackground>
+  )
+}
+
+const NormalRoundIntro = ({
   gameState,
   handleEndPhase
 }: {
