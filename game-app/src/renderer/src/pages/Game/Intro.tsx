04028cfc965326c966775652a0646cf0f09c9620
Lucas Watts <lucaswatts41@gmail.com>
2024-12-20 15:03:22 -0500

    wedkl

--- game-app/src/renderer/src/pages/Game/Intro.tsx
+++ game-app/src/renderer/src/pages/Game/Intro.tsx
@@ -2,6 +2,33 @@ import { DevButton } from '@renderer/components/DevButton'
 import { GradientBackground } from '@renderer/components/GradientBackground'
 import { Title } from '@renderer/components/Title'
 import { Game } from '@renderer/context/types'
+import poodleGif from '../../assets/gifs/poodle-dancing.gif'
+import styled from 'styled-components'
+
+const TextContainer = styled.div`
+  width: 70%;
+`
+
+const GameExplanation = styled.div`
+  font-family: 'Arvo';
+  font-size: 16px;
+  font-weight: 700;
+  text-align: center;
+  color: #fff;
+  margin-top: 20px;
+`
+
+const GifLeft = styled.img`
+  position: absolute;
+  left: 0;
+  top: 40%;
+`
+
+const GifRight = styled.img`
+  position: absolute;
+  right: 0;
+  top: 40%;
+`
 
 export const Intro = ({
   gameState,
@@ -28,7 +55,19 @@ const FirstRoundIntro = ({
 }) => {
   return (
     <GradientBackground>
-      <Title>Welcome to BEANS EXTREMES</Title>
+      <TextContainer>
+        <Title>Welcome to BEANS EXTREMES</Title>
+        <GameExplanation>
+          The game is simple:
+          <br />
+          Get a prompt - send the most extreme answer <br />
+          Check if it's extremely BAD or GOOD <br />
+          Vote for your favourite answer each round <br />
+          Get ready!!!
+        </GameExplanation>
+      </TextContainer>
+      <GifLeft src={poodleGif} style={{ transform: 'scaleX(-1)' }} />
+      <GifRight src={poodleGif} />
       <DevButton onClick={handleEndPhase}>End phase</DevButton>
     </GradientBackground>
   )
@@ -43,7 +82,9 @@ const NormalRoundIntro = ({
 }) => {
   return (
     <GradientBackground>
-      <p>Welcome to Beans Extremes!</p>
+      <GameExplanation>Get ready for the next round of BEANS EXTREMES</GameExplanation>
+      <GifLeft src={poodleGif} style={{ transform: 'scaleX(-1)' }} />
+      <GifRight src={poodleGif} />
       <DevButton onClick={handleEndPhase}>End phase</DevButton>
     </GradientBackground>
   )
