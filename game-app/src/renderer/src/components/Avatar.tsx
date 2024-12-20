d80bf08238d273c17f2da51b7be0ee5d329446b4
sisiandrew <andrew@sisiagency.com>
2024-12-20 15:53:21 -0500

    gitacp command

--- game-app/src/renderer/src/components/Avatar.tsx
+++ game-app/src/renderer/src/components/Avatar.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components'
+import bean1 from '../assets/avatars/bean1.png'
+import doni1 from '../assets/avatars/doni1.png'
+import frog1 from '../assets/avatars/frog1.png'
+import gilly1 from '../assets/avatars/gilly1.png'
+import mobin1 from '../assets/avatars/mobin1.png'
+import pipoca1 from '../assets/avatars/pipoca1.png'
 
 const avatars = [
-  { id: '1', avatar: '👨‍🦰' },
-  { id: '2', avatar: '👩‍🦰' },
-  { id: '3', avatar: '👨‍🦱' },
-  { id: '4', avatar: '👩‍🦱' },
-  { id: '5', avatar: '👨‍🦳' },
-  { id: '6', avatar: '👩‍🦳' },
-  { id: '7', avatar: '👨‍🦲' },
-  { id: '8', avatar: '👩‍🦲' }
+  { id: '1', avatar: doni1 },
+  { id: '2', avatar: bean1 },
+  { id: '3', avatar: mobin1 },
+  { id: '4', avatar: gilly1 },
+  { id: '5', avatar: pipoca1 },
+  { id: '6', avatar: frog1 }
 ]
 
 const StyledAvatar = styled.div<{ opacity?: number }>`
@@ -24,6 +28,12 @@ const Nickname = styled.p`
   text-align: center;
 `
 
+const StyledImage = styled.img`
+  width: 80px;
+  height: 80px;
+  margin: auto;
+`
+
 export const Avatar = ({
   avatarId,
   nickname,
@@ -33,10 +43,10 @@ export const Avatar = ({
   nickname: string
   opacity?: number
 }) => {
-  const avatar = avatars.find((avatar) => avatar.id === avatarId)?.avatar || ''
+  const avatar = avatars.find((avatar) => avatar.id === avatarId)?.avatar || null
   return (
     <StyledAvatar opacity={opacity}>
-      <Image>{avatar}</Image>
+      {avatar && <StyledImage src={avatar} alt={nickname} />}
       <Nickname>{nickname}</Nickname>
     </StyledAvatar>
   )
