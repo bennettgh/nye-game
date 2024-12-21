e9d28517328f6c32444c16a775eb2bde97147b94
sisiandrew <andrew@sisiagency.com>
2024-12-20 19:25:19 -0500

    gitacp command

--- game-server/index.ts
+++ game-server/index.ts
@@ -3,6 +3,7 @@ require("dotenv").config();
 import cors from "cors";
 import express, { Application } from "express";
 import * as http from "http";
+import { Server } from "socket.io";
 import { initIO } from "./io/io";
 
 const PORT: number = (process.env.PORT as any) || 8080;
@@ -11,11 +12,27 @@ const app: Application = express();
 app.use(cors());
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://comfy-maamoul-88a7d9.netlify.app",
+      "http://localhost:3000", // Common React dev port
+      "http://localhost:5173", // Common Vite dev port
+      "http://localhost:5174", // Common Vite dev port
+      "http://127.0.0.1:3000",
+      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5174",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-initIO(server);
+initIO(io);
 
 server
   .listen(PORT, function () {
