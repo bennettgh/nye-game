12f1236af72dfff37dd57fb03c620634c8962fe3
sisiandrew <andrew@sisiagency.com>
2024-12-20 19:04:20 -0500

    gitacp command

--- game-app/src/renderer/src/config.ts
+++ game-app/src/renderer/src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   backendURL:
     window.electron.process.env.NODE_ENV === 'development'
-      ? 'http://localhost:8080'
-      : 'https://express-backend-pib1.onrender.com'
+      ? 'https://nye-game.onrender.com:10000'
+      : 'https://nye-game.onrender.com:10000'
 }
