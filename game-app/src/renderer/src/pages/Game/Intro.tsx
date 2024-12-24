af7ed51eaaafd88f73b23f9f317d3aea3483e1db
Lucas Watts <lucaswatts41@gmail.com>
2024-12-23 21:36:54 -0500

    we

--- game-app/src/renderer/src/pages/Game/Intro.tsx
+++ game-app/src/renderer/src/pages/Game/Intro.tsx
@@ -1,26 +1,33 @@
 import { StarburstBackground } from '@renderer/components/backgrounds/Starburst1'
 import { DevButton } from '@renderer/components/DevButton'
 import { GradientBackground } from '@renderer/components/GradientBackground'
-import { Title } from '@renderer/components/Title'
 import { TriColorTitle } from '@renderer/components/titles/Tricolor'
+import { Bars } from '@renderer/components/backgrounds/Bars1'
 import { useSoundContext } from '@renderer/context/sound'
 import { Game } from '@renderer/context/types'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import poodleGif from '../../assets/gifs/poodle-dancing.gif'
+import { motion } from 'motion/react'
 
-const TextContainer = styled.div`
-  width: 70%;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
 `
 
-const GameExplanation = styled.div`
-  font-family: 'Arvo';
-  font-size: 30px;
-  font-weight: 700;
+const AnimatingTitle = styled(motion.h1)`
+  font-size: 2rem;
+  font-weight: bold;
+  font-family: 'Arvo', sans-serif;
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
   text-align: center;
-  color: #fff;
-  margin-top: 20px;
-  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+  line-height: 1.2;
+  z-index: 2;
+  width: 100%;
 `
 
 const GifLeft = styled.img`
@@ -61,34 +68,60 @@ const FirstRoundIntro = ({
   handleEndPhase: () => void
 }) => {
   const { playSound, stopSound } = useSoundContext()
+  const [showGameExplanation, setShowGameExplanation] = useState(false)
 
   useEffect(() => {
-    playSound('explanationMusic')
+    playSound('musicLobby')
 
     return () => {
-      stopSound('explanationMusic')
+      stopSound('musicLobby')
     }
   }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowGameExplanation(true)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <GradientBackground>
-      <TextContainer>
-        <Title>
-          WELCOME TO
-          <br />
-          BEANS EXTREMES
-        </Title>
-        <GameExplanation>
-          The game is simple:
-          <br />
-          Get a prompt - send the most extreme answer <br />
-          Check if it's extremely BAD or GOOD <br />
-          Vote for your favourite answer each round <br />
-          Get ready!!!
-        </GameExplanation>
-      </TextContainer>
-      <GifLeft src={poodleGif} style={{ transform: 'scaleX(-1)' }} />
-      <GifRight src={poodleGif} />
-      <DevButton onClick={handleEndPhase}>End phase</DevButton>
+      <Bars>
+        <Container>
+          {/* <TextContainer> */}
+          <AnimatingTitle
+            animate={{
+              opacity: [0, 1],
+              scale: [0.5, 1]
+            }}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut'
+            }}
+          >
+            How do you play?
+          </AnimatingTitle>
+          <AnimatingTitle
+            animate={{
+              opacity: [0, 1],
+              scale: [0.5, 1]
+            }}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 1.5
+            }}
+          >
+            Answer the prompt, vote for your favourite
+          </AnimatingTitle>
+          {/* </TextContainer> */}
+          <GifLeft src={poodleGif} style={{ transform: 'scaleX(-1)' }} />
+          <GifRight src={poodleGif} />
+        </Container>
+        <DevButton onClick={handleEndPhase}>End phase</DevButton>
+      </Bars>
     </GradientBackground>
   )
 }
